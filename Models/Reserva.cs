607a078c9d957da830134c645b5cba33271b0b93
Caio Abe <caioabe68@gmail.com>
2025-08-21 17:04:07 -0300

    feat: calcular valor da diária

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -45,14 +45,14 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            // *IMPLEMENTE AQUI* (CONCLUÍDO)
+            decimal valor = DiasReservados * Suite.ValorDiaria;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            // *IMPLEMENTE AQUI* (CONCLUÍDO)
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                valor -= valor * 10 / 100;
             }
 
             return valor;
